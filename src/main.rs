3fbd039bffe85ac3f78c71d5a642fb75001db7f1
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2021-12-02 10:35:30 +0100

    day2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,90 @@
 // most of this code is taken from https://fasterthanli.me/series/advent-of-code-2020/part-1
 // thanks amos!
+const INPUT: &str = include_str!("day2/input.txt");
+
+#[derive(Debug)]
+enum SubmarineCommandDirection {
+    Forward,
+    Down,
+    Up
+}
+
+#[derive(Debug)]
+struct SubmarineCommand {
+    direction: SubmarineCommandDirection,
+    value: i64
+}
+
 fn main() -> anyhow::Result<()> {
-    let s = count_number_of_increases(
-        include_str!("day1/input.txt")
+    let commands =
+        INPUT
             .split('\n')
-            .filter_map(|s| s.parse::<i64>().ok())
-            .collect()
-    );
+            .filter_map(|s| parse_submarine_command(s).ok())
+            .collect::<Vec<SubmarineCommand>>()
 
-    dbg!(s);
+    ;
 
-    let s2 = count_number_of_increases_in_windows(
-        include_str!("day1/input.txt")
-            .split('\n')
-            .filter_map(|s| s.parse::<i64>().ok())
-            .collect()
-    );
+    let (pos, depth) = compute_final_position(commands);
 
-    dbg!(s2);
+    let _ = !dbg!(pos * depth);
+
+    // let s2 = count_number_of_increases_in_windows(
+    //     INPUT
+    //         .split('\n')
+    //         .filter_map(|s| s.parse::<i64>().ok())
+    //         .collect()
+    // );
+
+    // dbg!(s2);
 
     Ok(())
 }
 
+fn compute_final_position(commands:Vec<SubmarineCommand>) -> (i64, i64) {
+    let mut horizontal_pos:i64 = 0;
+    let mut depth:i64 = 0;
 
-fn count_number_of_increases(s: Vec<i64>) -> i64 {
-    let mut previous_num = std::i64::MAX;
-    let mut increases = 0;
-    for i in 0..s.len() {
-        let num = s[i];
-        if num > previous_num{
-            increases += 1;
+    for command in commands.iter() {
+        match command {
+            SubmarineCommand {
+                direction: SubmarineCommandDirection::Down, value:v
+            } => depth += v,
+            SubmarineCommand {
+                direction: SubmarineCommandDirection::Up, value:v
+            } => depth -= v,
+            SubmarineCommand {
+                direction: SubmarineCommandDirection::Forward, value:v
+            } => horizontal_pos += v,
         }
-        previous_num = num
     }
-    return increases;
+
+    return (horizontal_pos, depth)
 }
 
-fn count_number_of_increases_in_windows(s: Vec<i64>) -> i64 {
-    let mut previous_num = std::i64::MAX;
-    let mut increases = 0;
-    for i in 2..s.len() {
+fn parse_submarine_command(s: &str) -> anyhow::Result<SubmarineCommand>{
+    let parts = s.trim().split_once(' ');
+    let direction: SubmarineCommandDirection;
+    let value: Result<i64, std::num::ParseIntError>;
 
-        let num = s[i] + s[i-1] + s[i-2];
-        if num > previous_num{
-            increases += 1;
-        }
-        previous_num = num
+    match parts {
+        Some(("forward", v)) => {
+            direction=SubmarineCommandDirection::Forward;
+            value=v.parse::<i64>()
+        },
+        Some(("up", v)) => {
+            direction=SubmarineCommandDirection::Up;
+            value=v.parse::<i64>()
+        },
+        Some(("down", v)) => {
+            direction=SubmarineCommandDirection::Down;
+            value=v.parse::<i64>()
+        },
+        _ => return Err(anyhow::anyhow!("Unable to parse: {}", s)),
+
     }
-    return increases;
+
+    match value{
+        Ok(v) => return Ok(SubmarineCommand{direction:direction, value:v}),
+        Err(_) => return Err(anyhow::anyhow!("Unable to parse: {}", s)),
+    }
 }
